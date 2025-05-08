a60da820124d22b16904f829934508fd957e2141
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-05-08 10:47:10 +0600

    add details

--- incorrect_regex.py
+++ incorrect_regex.py
@@ -1,4 +1,7 @@
 # Output: True if the string is a valid regex, False otherwise.
+"""
+To check if a regular expression pattern is valid in Python before using it for matching, the re.compile() function can be used within a try-except block. If the pattern is invalid, it will raise a re.error exception, which can be caught and handled.
+"""
 import re
 def is_valid_regex(s):
     try:
